bf3b713cb0cc406051a57327d7218209dc41b4c7
ChenLu-china <74493433+ChenLu-china@users.noreply.github.com>
2023-02-02 10:27:29 +0800

    Update setup.py

--- cudaC++/advance/csrc/setup.py
+++ cudaC++/advance/csrc/setup.py
@@ -1,1 +1,18 @@
 
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='luchen',
+    ext_modules=[
+        CUDAExtension('luchen', [
+            '/home/chenlu/projects/chenlu/cuda_c++/cuda/csrc/svox2.cpp',
+            '/home/chenlu/projects/chenlu/cuda_c++/cuda/csrc/svox2_kernel.cu',
+            '/home/chenlu/projects/chenlu/cuda_c++/cuda/csrc/misc_kernel.cu',
+            '/home/chenlu/projects/chenlu/cuda_c++/cuda/csrc/render_lerp_kernel_cuvol.cu',
+            '/home/chenlu/projects/chenlu/cuda_c++/cuda/csrc/rende_depth_kernel.cu'
+        ]),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
